43dd5b1f747e7683b4abd921cf5e2b9b0f123fde
Tiago Pinto <tp@tiagopinto.pt>
2021-08-25 12:24:03 +0100

    never do a huge commit like this

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # /restaurants/...
+  resources :restaurants do
+
+    # .../reviews/...
+    resources :reviews, only: [ :new, :create ]
+
+    collection do
+
+      # .../top
+      get :top
+
+      # .../bottom
+      get :bottom
+    end
+
+    member do
+
+      # .../chef
+      get :chef
+    end
+  end
+
+  resources :reviews, only: [ :destroy ]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", stars: 5 }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", stars: 4 }
+
+all_chefs = [ "Jamie", "Blumenthal" ]
+
+[ dishoom, pizza_east ].each do |attributes|
+    
+  attributes[:chef_name] = all_chefs.sample
+
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
